a4f669449342f9103eb432ec37900eaeeb07e4e6
redquilt <redquilt@163.com>
2021-07-08 16:55:51 +0800

    精修，找到新的范围： U=0.512-0.519 m=110-155

--- find_m_U.m
+++ find_m_U.m
@@ -1,12 +1,12 @@
 %高维
-start_U=0.50;
-end_U=0.54;
+start_U=0.515;
+end_U=0.515;
 step_U=0.001;
 %低维
-start_m=120;
-end_m=120;
+start_m=85;
+end_m=160;
 step_m=5;
-%结论:U=0.512-0.519,m=95-140
+%结论:U=0.512-0.519,m=110-155
 
 results=zeros(round((end_U-start_U)/step_U)+1,round((end_m-start_m)/step_m)+1);
 for U = start_U:step_U:end_U
